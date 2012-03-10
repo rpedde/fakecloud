51ac0df3ada5942211d61cc65e3eb068eaa5c58b
rpedde <ron@pedde.com>
2012-03-09 17:31:54 -0800

    error handling fixups

--- virt.sh
+++ virt.sh
@@ -359,7 +359,12 @@ function maybe_make_dist_image() {
     arch=amd64
     tmpdir=$(mktemp -d)
 
-    trap "set +e; rm -rf ${tmpdir}; return 1" SIGINT SIGTERM ERR
+    function maybe_make_dist_image_cleanup() {
+	rm -rf ${tmpdir}
+	return 1
+    }
+
+    trap "maybe_make_dist_image_cleanup; return 1" SIGINT SIGTERM ERR
 
     for l in ${LIB_DIR}/os/{default,$dist/default,$dist/$release}; do
 	if [ -f $l ]; then
